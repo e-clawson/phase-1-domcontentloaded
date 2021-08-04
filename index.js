ec478ae633124623150555cfc3ad3a606776efbe
Elizabeth Clawson <emclawson1@gmail.com>
2021-08-03 20:14:09 -0500

    commit tree 8a1c80200515d751ad86d1c1cde923b52826cf24

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+window.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript is so cool. It lets me add text to my page programmatically." );
+  }); 
+
+// console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   ); 
